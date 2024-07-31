5cf2102a02d2befecc4f11a52d91c5201ed05dd2
shoban12 <45650006+shoban12@users.noreply.github.com>
2024-07-30 22:38:15 -0400

    initial code

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      pageNum: 1,
+      totalPages: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.changePage(this.state.pageNum);
+  }
+
+  changePage(pageNum) {
+    const url = `https://reqres.in/api/users?page=${pageNum}`;
+    
+    fetch(url)
+    .then((res) => res.json())
+    .then((res) => this.setState({ users: res.data, pageNum: pageNum, totalPages: res.total_pages }));
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Users List:</h1>
+        </div>
+
+        <div className="users">
+          {users.map((user) => (
+            <div className="card" key="{user.id}">
+              <img
+                src={user.avatar}
+                className="card-img-top"
+                alt={`${user.email} avatar`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {user.first_name} {user.last_name}
+                </h5>
+                <p className="card-text">{user.email}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button type="button" disabled={this.state.pageNum ===1 ? true : false} className="btn-secondary" onClick={() =>this.changePage(this.state.pageNum -1)}>Prev</button>
+          <button type="button" className="btn-secondary">{this.state.pageNum}</button>
+          <button type="button" disabled= {this.state.pageNum >= this.state.totalPages ? true: false} className="btn-secondary" onClick={() =>this.changePage(this.state.pageNum +1)}>Next</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
